2d332b072d2cb739f60544842527e8bb68dc0dac
RUSHES02 <abhijaanganguly02@gmail.com>
2025-07-04 13:11:19 +0530

    api call

--- app/src/main/java/com/razorpay/sampleapp/api.kt
+++ app/src/main/java/com/razorpay/sampleapp/api.kt
@@ -1,5 +1,7 @@
 package com.razorpay.sampleapp
 
+import com.razorpay.sampleapp.networking.NetworkError
+import com.razorpay.sampleapp.networking.Result
 import com.razorpay.sampleapp.networking.constructUrl
 import com.razorpay.sampleapp.networking.safeCall
 import io.ktor.client.HttpClient
@@ -9,7 +11,7 @@ import io.ktor.client.request.setBody
 class Api(
     private val httpClient: HttpClient,
 ) {
-    suspend fun paymentOrder(id: String, request: CreatePaymentOrderRequest): OrderResponse {
+    suspend fun paymentOrder(id: String, request: CreatePaymentOrderRequest): Result<OrderResponse, NetworkError> {
         return safeCall<OrderResponse> {
             httpClient.patch(
                 urlString = constructUrl("/reservations/${id}/status")
